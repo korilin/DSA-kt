3291577a804b81f6f9969fe6a2a544c44bef4c73
korilin <korilin.dev@gmail.com>
2021-03-15 11:24:34 +0800

    remove shellsort time complexity

--- src/main/kotlin/sort_algorithm/shellSort.kt
+++ src/main/kotlin/sort_algorithm/shellSort.kt
@@ -3,14 +3,16 @@ package sort_algorithm
 /**
  * 希尔排序（递减增量排序）
  * 时间复杂度：
- *   最优时间复杂度：Ο(n)
- *   最坏时间复杂度：最好的情况为 Ο(n*log²(n))
+ *   最优时间复杂度：
+ *   平均时间复杂度：
+ *   最坏时间复杂度：
  * 空间复杂度：Ο(1)
  *
  * Shell Sort
  * Time Complexity:
- *   Optimal Time Complexity: Ο(n)
- *   Worst Time Complexity: the best case is Ο(n*log²(n))
+ *   Optimal Time Complexity:
+ *   Average Time Complexity:
+ *   Worst Time Complexity:
  * Space Complexity: Ο(1)
  */
 fun shellSort(array: IntArray){
