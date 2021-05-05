4e682eeb2b6224efba4599e09c225a3384ac4d7e
korilin <korilin.dev@gmail.com>
2021-05-05 15:10:32 +0800

    完成红黑树删除操作
    使用自顶向下的解决方案完成红黑树删除

--- src/main/kotlin/data_structure/RBTree.kt
+++ src/main/kotlin/data_structure/RBTree.kt
@@ -20,24 +20,6 @@ class RBTree<E> {
 
     private var root: Node<E>? = null
 
-    fun add(key: Int, value: E) {
-        val newNode = Node(key, value, null, null, null, Color.RED)
-        if (insertNodeIntoTree(newNode))  balanceControl(newNode)
-    }
-
-    fun get(key: Int): E? {
-        var compareNode = root
-        while (compareNode != null) {
-            if (compareNode.key == key) return compareNode.value
-            compareNode = if (key < compareNode.key) compareNode.left else compareNode.right
-        }
-        return null
-    }
-
-    fun remove(e: Int) {
-        TODO()
-    }
-
     /**
      * 打印树的结构（包括 NIL 节点）
      */
@@ -66,6 +48,13 @@ class RBTree<E> {
         dfs(root, 0)
     }
 
+    /**
+     * 添加元素，当对应 key 的节点存在时，将覆盖原有节点的 value
+     */
+    fun add(key: Int, value: E) {
+        val newNode = Node(key, value, null, null, null, Color.RED)
+        if (insertNodeIntoTree(newNode)) balanceControl(newNode)
+    }
 
     /**
      * 将节点插入的红黑树中，当对应 key 的节点存在时，使用覆盖原本节点的 value，而不是进行插入操作
@@ -101,6 +90,7 @@ class RBTree<E> {
 
     /**
      * 对当前操作的红色节点进行平衡处理
+     * 自底向上处理方案
      */
     private fun balanceControl(node: Node<E>) {
         val father = node.father
@@ -124,7 +114,7 @@ class RBTree<E> {
     }
 
     /**
-     * 进行旋转操作的选择
+     * 进行旋转操作的方向选择
      */
     private fun rotate(node: Node<E>, father: Node<E>, grandfather: Node<E>) {
         if (grandfather.left == father) {
@@ -147,11 +137,159 @@ class RBTree<E> {
     }
 
     /**
+     * 获取对应 key 的元素
+     */
+    fun get(key: Int): E? {
+        var compareNode = root
+        while (compareNode != null) {
+            if (compareNode.key == key) return compareNode.value
+            compareNode = if (key < compareNode.key) compareNode.left else compareNode.right
+        }
+        return null
+    }
+
+    /**
+     * 移除元素
+     */
+    fun remove(key: Int): E? {
+
+        // 处理 root 为 2-node 的情况
+        root?.let {
+            if (is2NodeHelper(it)) {
+                val left = it.left
+                val right = it.right
+                if (left != null && is2NodeHelper(left) && is2NodeHelper(right!!)) {
+                    left.color = Color.RED
+                    right.color = Color.RED
+                }
+            }
+        } ?: return null
+
+        var current = root
+
+        // 找到要删除的节点
+        while (current != null) {
+            if (current.key == key) break
+            current = if (key < current.key) combine2NodeHelper(current, current.left, current.right)
+            else combine2NodeHelper(current, current.right, current.left)
+        }
+
+        val node = current ?: return null
+        val value = node.value
+
+        // 寻找替换节点
+        current = searchReplaceNode(current)
+
+        node.key = current.key
+        node.value = current.value
+        // 删除替换节点
+        deleteNode(current)
+
+        return value
+    }
+
+    private fun searchReplaceNode(node: Node<E>): Node<E> {
+        var current = node
+        if (current.left != null) {
+            current = combine2NodeHelper(current, current.left, current.right)!!
+            while (current.right != null)
+                current = combine2NodeHelper(current, current.right, current.left)!!
+        } else {
+            current = combine2NodeHelper(current, current.right, current.left)!!
+            while (current.left != null)
+                current = combine2NodeHelper(current, current.left, current.right)!!
+        }
+        return current
+    }
+
+    private fun deleteNode(node: Node<E>) {
+        val father = node.father
+        when {
+            father == null -> root = null
+            father.left == node -> father.left = null
+            father.right == node -> father.right = null
+        }
+        node.father = null
+    }
+
+    private fun is2NodeHelper(node: Node<E>): Boolean {
+        return node.left?.color != Color.RED && node.right?.color != Color.RED
+    }
+
+    /**
+     * 自顶向下合并 2-node 的节点删除平衡保持解决方案
+     *
+     * @param current 当前遍历的节点
+     * @param next 下一个查找节点
+     * @param border next的兄弟节点
+     * @return next
+     */
+    private fun combine2NodeHelper(current: Node<E>, next: Node<E>?, border: Node<E>?): Node<E>? {
+        if (next != null && is2NodeHelper(next)) {
+            border!!
+            if (is2NodeHelper(border)) {
+                // combine siblings
+                current.color = Color.BLACK
+                border.color = Color.RED
+                next.color = Color.RED
+            } else {
+                // borrow from sibling
+                borrowFromSiblingBefore(current, next, border)
+            }
+        }
+        return next
+    }
+
+    /**
+     * borrow from sibling 预处理
+     */
+    private fun borrowFromSiblingBefore(current: Node<E>, next: Node<E>, border: Node<E>) {
+
+        fun samePathVerity(it: Node<E>, rotate: (node: Node<E>) -> Unit) {
+            if (it.color == Color.RED) it.color =
+                Color.BLACK.also { borrowFromSiblingImprove(current, next, border, rotate) }
+        }
+
+        fun differentPathInvoke(borderRotate: (node: Node<E>) -> Unit, currentRotate: (node: Node<E>) -> Unit) {
+            borderRotate(border).also { borrowFromSiblingImprove(current, next, border, currentRotate) }
+        }
+
+        when (border) {
+            current.left -> if (border.right?.color == Color.RED)
+                differentPathInvoke(::leftRotate, ::rightRotate)
+            else border.left?.let {
+                samePathVerity(it, ::rightRotate)
+            }
+
+            current.right -> if (border.left?.color == Color.RED)
+                differentPathInvoke(::rightRotate, ::leftRotate)
+            else border.right?.let {
+                samePathVerity(it, ::leftRotate)
+            }
+        }
+    }
+
+    /**
+     * borrow from sibling improve
+     */
+    private fun borrowFromSiblingImprove(
+        current: Node<E>,
+        next: Node<E>,
+        border: Node<E>,
+        rotate: (node: Node<E>) -> Unit
+    ) {
+        border.color = current.color
+        current.color = Color.BLACK
+        next.color = Color.RED
+        rotate(current)
+    }
+
+    /**
      * 以传入的节点为中心进行左旋操作
      */
     private fun leftRotate(node: Node<E>) {
         val right = node.right!!
-        childChange(node.father, node, right)
+        relationChange(node.father, node, right)
         node.right = right.left
         right.left = node
     }
@@ -161,7 +299,7 @@ class RBTree<E> {
      */
     private fun rightRotate(node: Node<E>) {
         val left = node.left!!
-        childChange(node.father, node, left)
+        relationChange(node.father, node, left)
         node.left = left.right
         left.right = node
     }
@@ -169,7 +307,7 @@ class RBTree<E> {
     /**
      * 旋转操作时，修改父节点的子节点指向
      */
-    private fun childChange(father: Node<E>?, child: Node<E>, newChild: Node<E>) = when {
+    private fun relationChange(father: Node<E>?, child: Node<E>, newChild: Node<E>) = when {
         father == null -> root = newChild
         father.left == child -> father.left = newChild
         else -> father.right = newChild
--- src/test/kotlin/StructureTest.kt
+++ src/test/kotlin/StructureTest.kt
@@ -6,9 +6,14 @@ class StructureTest {
     @Test
     fun rbTreeTest(){
         val rbTree = RBTree<Int>()
+        var i = 0
         repeat(10){
-            rbTree.add(1, 50)
+            rbTree.add(i, 50)
+            i++
         }
+        rbTree.printTree()
+        println("=====================")
+        rbTree.remove(7)
         rbTree.printTree()
     }
 }
