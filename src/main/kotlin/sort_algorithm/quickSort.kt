b96ba1b2998b0c6b057fc9b77f677a3183c436ed
korilin <korilin.dev@gmail.com>
2021-08-23 14:09:51 +0800

    Update quickSort.kt

--- src/main/kotlin/sort_algorithm/quickSort.kt
+++ src/main/kotlin/sort_algorithm/quickSort.kt
@@ -17,20 +17,19 @@ package sort_algorithm
  */
 fun quickSort(array: IntArray) {
 
-    fun inner(left: Int, right: Int) {
-
-        fun partition(left: Int, right: Int): Int {
-            // 以最左边的值作为基准值
-            var pivotPreIndex = left
-            for (index in left..right) {
-                if (array[index] <= array[right]) {
-                    array[pivotPreIndex] = array[index].also { array[index] = array[pivotPreIndex] }
-                    pivotPreIndex += 1
-                }
+    fun partition(left: Int, right: Int): Int {
+        // 以最左边的值作为交换值
+        var pivotPreIndex = left
+        for (index in left..right) {
+            if (array[index] <= array[right]) {
+                array[pivotPreIndex] = array[index].also { array[index] = array[pivotPreIndex] }
+                pivotPreIndex += 1
             }
-            return pivotPreIndex - 1
         }
+        return pivotPreIndex - 1
+    }
 
+    fun inner(left: Int, right: Int) {
         if (left < right) {
             val p = partition(left, right)
             inner(left, p - 1)
